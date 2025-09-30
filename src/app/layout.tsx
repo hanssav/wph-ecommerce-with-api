124104434d24968ff9ad1fd3675c78296aa23ec4
hanssav <handihandi40@gmail.com>
2025-09-30 14:50:14 +0700

    apply font-sf
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,33 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import localFont from 'next/font/local';
+
+const sfProDisplay = localFont({
+  src: [
+    {
+      path: '../../public/fonts/SF-Pro-Display-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SF-Pro-Display-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SF-Pro-Display-Semibold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SF-Pro-Display-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-sf-pro-display',
+});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +52,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${sfProDisplay.variable} antialiased`}
       >
         {children}
       </body>
