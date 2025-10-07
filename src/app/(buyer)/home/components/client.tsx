52bbd3f5e50b270c20ab40d2363258d811cadbc2
hanssav <handihandi40@gmail.com>
2025-10-07 11:30:20 +0700

    feat: use ssr in home page
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/app/(buyer)/home/components/client.tsx
+++ src/app/(buyer)/home/components/client.tsx
@@ -1,7 +1,46 @@
+'use client';
 import React from 'react';
+import ProductCard from '@/components/container/product-card';
+import ProductCardSkeleton from '@/components/container/product-card-skeleton';
+import { Button } from '@/components/ui/button';
+import Typography from '@/components/ui/typography';
+import { useInfiniteProducts } from '@/hooks';
 
 const HomeClient = () => {
-  return <div>HomeClient</div>;
+  const limit = 8;
+  const {
+    products,
+    query: { fetchNextPage, hasNextPage, isFetchingNextPage, isLoading },
+  } = useInfiniteProducts({ limit: limit });
+
+  return (
+    <>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+
+        {(isLoading || isFetchingNextPage) &&
+          Array.from({ length: limit }).map((_, index) => (
+            <ProductCardSkeleton key={`skeleton-${index}`} />
+          ))}
+      </div>
+      <div className='flex justify-center w-full'>
+        <Button
+          variant={'outline'}
+          size={'lg'}
+          className='rounded-lg w-40 lg:!w-[220px]'
+          onClick={() => fetchNextPage()}
+          disabled={isFetchingNextPage || !hasNextPage}
+        >
+          <Typography size={{ base: 'md' }} weight={'semibold'}>
+            {isFetchingNextPage ? 'Loading...' : 'Load More'}
+          </Typography>
+        </Button>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default HomeClient;
--- src/app/(buyer)/home/page.tsx
+++ src/app/(buyer)/home/page.tsx
@@ -1,25 +1,44 @@
-'use client';
-import ProductCard from '@/components/container/product-card';
-import { Button } from '@/components/ui/button';
-import Typography from '@/components/ui/typography';
-import { useInfiniteProducts } from '@/hooks';
-import React from 'react';
-import HeroSection from './components/hero-section';
+import {
+  dehydrate,
+  QueryClient,
+  DehydratedState,
+  HydrationBoundary,
+} from '@tanstack/react-query';
+import { productsService } from '@/services';
+import { ParamsProduct, ProductApiResponse } from '@/types';
 import SectionWrapper from '@/components/container/section-wrapper';
-import ProductCardSkeleton from '@/components/container/product-card-skeleton';
+import HeroSection from './components/hero-section';
+import Typography from '@/components/ui/typography';
+import HomeClient from './components/client';
 
-export default function Home() {
-  const limit = 8;
-  const {
-    products,
-    query: { fetchNextPage, hasNextPage, isFetchingNextPage, isLoading },
-  } = useInfiniteProducts({ limit: limit });
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery<
+    ProductApiResponse,
+    Error,
+    ProductApiResponse,
+    [string, ParamsProduct],
+    number
+  >({
+    queryKey: ['products', {} as ParamsProduct],
+    queryFn: async ({ pageParam = 1 }) => {
+      return productsService.getAll({ page: pageParam, limit: 20 });
+    },
+    getNextPageParam: (lastPage: ProductApiResponse) => {
+      const { page, totalPages } = lastPage.data.pagination;
+      return page < totalPages ? page + 1 : undefined;
+    },
+    initialPageParam: 1,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <SectionWrapper className='px-4 py-6 lg:px-[120px] lg:py-12'>
       <HeroSection />
 
-      <section className='flex flex-col gap-3 lg:gap-10 '>
+      <section className='flex flex-col gap-3 lg:gap-10'>
         <Typography
           as='h2'
           size={{ base: 'display-xs', lg: 'display-lg' }}
@@ -27,31 +46,9 @@ export default function Home() {
         >
           Feature Product
         </Typography>
-
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-
-          {(isLoading || isFetchingNextPage) &&
-            Array.from({ length: limit }).map((_, index) => (
-              <ProductCardSkeleton key={`skeleton-${index}`} />
-            ))}
-        </div>
-
-        <div className='flex justify-center w-full'>
-          <Button
-            variant={'outline'}
-            size={'lg'}
-            className='rounded-lg w-40 lg:!w-[220px]'
-            onClick={() => fetchNextPage()}
-            disabled={isFetchingNextPage || !hasNextPage}
-          >
-            <Typography size={{ base: 'md' }} weight={'semibold'}>
-              {isFetchingNextPage ? 'Loading...' : 'Load More'}
-            </Typography>
-          </Button>
-        </div>
+        <HydrationBoundary state={dehydratedState}>
+          <HomeClient />
+        </HydrationBoundary>
       </section>
     </SectionWrapper>
   );
