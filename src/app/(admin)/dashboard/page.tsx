7ce1aa4d01e9a1cb5290b78b7a8faf5b11628349
hanssav <handihandi40@gmail.com>
2025-10-07 20:17:38 +0700

    dashbord ui
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,28 @@
+import { Card } from '@/components/ui/card';
+import Typography from '@/components/ui/typography';
+import TypographyTitle from '@/components/ui/typography/Title';
+import DASHBOARD_CONSTANT from '@/constants/dashboard';
 import React from 'react';
 
 const Dashboard = () => {
-  return <div className='flex h-fullfont-bold'>di sini Dashboard</div>;
+  return (
+    <div className='h-full flex flex-col gap-3'>
+      <TypographyTitle label='Dashboard' />
+      <div className='flex flex-col lg:flex-row gap-2 lg:gap-3'>
+        {DASHBOARD_CONSTANT.map(({ title, data, Icon }, idx) => (
+          <Card key={idx} className='p-3 flex flex-col gap-2 lg:flex-1'>
+            <Icon className='w-5 h-5' />
+            <Typography weight={'normal'} size={{ base: 'sm' }}>
+              {title}
+            </Typography>
+            <Typography weight={'bold'} size={{ base: 'xl', lg: 'display-xs' }}>
+              {data}
+            </Typography>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/constants/dashboard.ts
@@ -0,0 +1,35 @@
+import {
+  ShoppingBag,
+  ClipboardList,
+  CheckCircle,
+  CircleDollarSign,
+} from 'lucide-react';
+
+const DASHBOARD_CONSTANT: {
+  title: string;
+  data: number | string;
+  Icon: React.ComponentType<any>;
+}[] = [
+  {
+    title: 'Total Products',
+    data: 128,
+    Icon: ShoppingBag,
+  },
+  {
+    title: 'Total Orders',
+    data: 342,
+    Icon: ClipboardList,
+  },
+  {
+    title: 'Total Revenue',
+    data: 'Rp1.920.000',
+    Icon: CircleDollarSign,
+  },
+  {
+    title: 'Completed Orders',
+    data: 298,
+    Icon: CheckCircle,
+  },
+];
+
+export default DASHBOARD_CONSTANT;
